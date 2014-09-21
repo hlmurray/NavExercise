27209d554e7d11d543e3180aa2951f5f65db92ab
Hannah Murray <hlmurray91@gmail.com>
2014-09-21 15:20:19 -0400

    feat(NavExercise): complete slide in mobile nav

--- public/js/index.js
+++ public/js/index.js
@@ -10,10 +10,10 @@
 var listItems = document.getElementsByTagName('li');
 
 for(var j=0; j<listItems.length; j++) {
-	listItems[j].addEventListener('click', Toggle);
+	listItems[j].addEventListener('click', toggle);
 }
 
-function Toggle() {
+function toggle() {
 	var nested = this.querySelectorAll('.nested-list')[0];
 	if(nested.classList){
 		nested.classList.toggle('show');
@@ -21,4 +21,23 @@ function Toggle() {
 	else if(nested.style.display === 'none' || ''){
 		nested.classList.add('show');
 	}
+}
+
+//functions to handle nav display in mobile view
+
+$(function() {
+    $('.toggle-nav').click(function() {
+        // Calling a function in case you want to expand upon this.
+        toggleNav();
+    });
+});
+
+function toggleNav() {
+    if ($('.site-wrapper').hasClass('show-nav')) {
+        // Do things on Nav Close
+        $('.site-wrapper').removeClass('show-nav');
+    } else {
+        // Do things on Nav Open
+        $('.site-wrapper').addClass('show-nav');
+    }
 }
