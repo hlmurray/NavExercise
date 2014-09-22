8aae70e2979453e6f08d2b496e2d311e670ee2c8
Hannah Murray <hlmurray91@gmail.com>
2014-09-21 20:17:51 -0400

    feat(NavExercise): implement desktop nav mask

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,68 @@
- /* $.ajax({
-        type: "GET",
-        url: "../data/nav.json",
-        dataType: "json",
-        success: function (xml) {
-            alert(data);
-        },
-    }); */
-
-var listItems = document.getElementsByTagName('li');
-
-for(var j=0; j<listItems.length; j++) {
-	listItems[j].addEventListener('click', toggle);
+function getMenuItemsFromApi() {
+	$.ajax("/api/nav.json", {
+		success: function(d) {
+			var navigation = createNestedList(d);
+			// append to DOM
+			var primary = document.getElementsByClassName('primary')[0];
+			primary.appendChild(navigation);
+			addEventListeners();
+		}
+	});
 }
 
-function toggle() {
-	var nested = this.querySelectorAll('.nested-list')[0];
-	if(nested.classList){
-		nested.classList.toggle('show');
+function createNestedList(d){
+	var navigation = document.createDocumentFragment();
+	
+	for(var j=0, jsonLength=d.items.length; j<jsonLength; j++) {
+		var newListItem = document.createElement("li"),
+			anchor = document.createElement("a"),
+			nestedItems = d.items[j].items;
+
+		// create list items	
+		anchor.href = d.items[j].url;
+		anchor.innerHTML = d.items[j].label;
+		newListItem.appendChild(anchor);
+
+		// create secondary nav items
+		if (nestedItems){
+			if (nestedItems.length > 0){
+				var ul = document.createElement('ul');
+				ul.classList.add('secondary');
+				var nestedList = createNestedList(d.items[j]);
+				if(nestedList) {
+					ul.appendChild(nestedList);
+					newListItem.appendChild(ul);
+					//newListItem.classList.add('chevron');
+				}
+			}
+		}	
+		navigation.appendChild(newListItem);
 	}
-	else if(nested.style.display === 'none' || ''){
-		nested.classList.add('show');
+	return navigation;
+}
+
+function addEventListeners(){
+	var nestedNavs = document.querySelectorAll('.secondary');
+	for(var j=0; j<nestedNavs.length; j++) {
+		nestedNavs[j].parentNode.addEventListener('click', ToggleNavElements);
+			
+	}
+}
+
+function ToggleNavElements() {
+	this.classList.toggle('nestedlist');
+	if ($('.mask').hasClass('show-mask')) {
+		$('.mask').removeClass('show-mask');
+	} else {
+		$('.mask').addClass('show-mask');
 	}
 }
 
 //functions to handle nav display in mobile view
 
 $(function() {
+	getMenuItemsFromApi();
+	
     $('.toggle-nav').click(function() {
         // Calling a function in case you want to expand upon this.
         toggleNav();
